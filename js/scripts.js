e2c8136f78bd92c4fc5fe18b5975d7a633c5bf1f
mario70pt <mario70pt@users.noreply.github.com>
2017-05-08 19:09:01 +0200

    dodanie liczby porządkowej do 'item' w elem.'li'

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,56 @@
+//uchwyt elementu button o id="addElem"
 var btnClik = document.querySelector('#addElem');
-
-function createItem(e) {
-//pierwszy sposób
+//funkcja dla tworzenia elementów "li" wraz z zagnieżdżaniem "item x"
+function onClik () {
+    //uchwyt elementu o id="list"
+    var newlist = document.querySelector('#list');
+    //tworzenie elementów "li"
     var newElemLi = document.createElement("li");
+    //określenie indeksu porządkowego "li"
+    var numLi = document.getElementsByTagName('li').length;
+    //element "li" -> "list"        
+    newlist.appendChild(newElemLi);
+    //element "item" + liczba index porządkowy -> "li" 
+    newElemLi.appendChild(document.createTextNode("item " + numLi++));    
+}
+//monitoring po kliknięciu myszką na button 'btnClik'wykonuje się wszystko z funkcji 'onClik'
+btnClik.addEventListener('click', onClik);
+
+
+
+
+
+
+
+
+
+
+
+/*var btnClik = document.querySelector('#addElem');
+
+function createItem() {
+//pierwszy sposób
+    var newlist = document.getElementById("list");
+    var newElemLi = document.createElement("li");
+    
     var newItem = document.createTextNode("item ");
+        
     newElemLi.appendChild(newItem);
-    document.getElementById("list").appendChild(newElemLi);
+        
+    var num = document.getElementsByTagName('li').length;
+    console.log(num);
+    
+    newlist.appendChild(newElemLi);
+    */
 //albo
 //drugi sposób
-    /*var list = document.getElementById("list");
-    list.innerHTML += '<li>item</li>'; */
-}
+    /*var newlist = document.getElementById("list");
+    
+    var num = document.getElementsByTagName('li').length;
+    console.log(num);
+    newlist.innerHTML += '<li>item</li>';
+}*/
 
-btnClik.addEventListener('click', createItem);
+//btnClik.addEventListener('click', createItem);
 
 //tylko nie wiem jak dopisać liczbę porządkową do item...:(
-    
